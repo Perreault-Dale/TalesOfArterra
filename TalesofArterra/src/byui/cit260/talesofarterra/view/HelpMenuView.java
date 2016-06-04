6998ca31d310dba23cebc18d1cd6ae8e7e61eaf9
RodgersLucas <rod15031@byui.edu>
2016-06-03 23:05:11 -0600

    Update HelpMenuView.java

--- TalesofArterra/src/byui/cit260/talesofarterra/view/HelpMenuView.java
+++ TalesofArterra/src/byui/cit260/talesofarterra/view/HelpMenuView.java
@@ -4,22 +4,15 @@
  * and open the template in the editor.
  */
 package byui.cit260.talesofarterra.view;
-
+import java.util.Scanner;
+//import java.io.*;
 /**
  *
  * @author Lucas
  */
 public class HelpMenuView {
     
-    private String promptListMember;
-    
-    public void helpMenuView()
-    {
-        this.displayMenu();
-        this.promptListMember = "\nPlease select an option: ";
-    }
-    
-    private void displayMenu()
+    public void displayMenu()
     {
         System.out.println(
             "\n****************************************************************"
@@ -34,4 +27,63 @@ public class HelpMenuView {
           + "\n*       \"B\"..................................BACK TO GAME    *"
           + "\n****************************************************************");
     }
+
+    public void menuChoice()
+    {
+        displayMenu();
+        char choice;
+        do
+        {
+           System.out.print( "Please enter an option: " );
+           Scanner reader = new Scanner(System.in);
+           choice = reader.next(".").charAt(0);
+
+           switch(choice)
+           {
+            case 'O':
+            case 'o': objective();
+                      break;
+            case 'M':
+            case 'm': movement();
+                      break;
+            case 'H':
+            case 'h': henchmen();
+                          break;
+            case 'R':
+            case 'r': resting();
+                          break;
+            case 'B':
+            case'b': resumeGame();
+                          break;
+            default: System.out.println( "ERROR: That is not a valid choice!" );
+            }
+           }
+           while (choice != 'o' || choice != 'O' 
+                   || choice != 'm' || choice != 'M'
+                   || choice != 'h' || choice != 'H'
+                   || choice != 'r' || choice != 'R'
+                   || choice != 'b' || choice != 'B');
+
+    }
+
+    private void objective() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void movement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void henchmen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void resting() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void resumeGame() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
+ 
