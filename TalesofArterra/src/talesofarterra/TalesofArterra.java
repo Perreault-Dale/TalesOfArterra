918887a058a9b3c5831777c8d2e773bc0df7c75d
Anubis <Anubis>
2016-06-03 23:37:11 -0400

    sigh changing things back, and i will work on main menu since you guys did the others

--- TalesofArterra/src/talesofarterra/TalesofArterra.java
+++ TalesofArterra/src/talesofarterra/TalesofArterra.java
@@ -7,8 +7,8 @@ package talesofarterra;
 
 import byui.cit260.talesofarterra.model.Game;
 import byui.cit260.talesofarterra.model.Player;
-//import byui.cit260.talesofarterra.view.StartMenuView;
-import byui.cit260.talesofarterra.view.Welcome;
+import byui.cit260.talesofarterra.view.StartMenuView;
+
 
 
 import java.util.Scanner;
@@ -24,24 +24,9 @@ public class TalesofArterra {
     
     
     public static void main(String[] args) {
-        /*StartMenuView startMenuView = new StartMenuView();
+        StartMenuView startMenuView = new StartMenuView();
         startMenuView.displayMenuView();
-        */
-        
-        Scanner input = new Scanner(System.in);
-                
-		Welcome welcomeObject = new Welcome();
-		//User input
-                welcomeObject.displayBanner();
-		System.out.println("By what name shall thy glory be remembered?: ");
-		//temp value from user
-		String tempName = input.nextLine();
-		//sends the temp name to welcome class 
-		welcomeObject.setPlayerName(tempName);
-		//gets the output from welcome class with the inputed name.
-		welcomeObject.display();
-                System.out.println();
-        
+         
     }
     
     
