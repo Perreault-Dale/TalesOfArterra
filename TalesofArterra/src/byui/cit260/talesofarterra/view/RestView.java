9475a0c31e7f185161f79aec2721eda430b62915
RodgersLucas <rod15031@byui.edu>
2016-06-18 21:06:36 -0600

    Started Rest View

--- TalesofArterra/src/byui/cit260/talesofarterra/view/RestView.java
+++ TalesofArterra/src/byui/cit260/talesofarterra/view/RestView.java
@@ -1,1 +1,37 @@
-dd
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.talesofarterra.view;
+
+//import java.util.Scanner;
+import byui.cit260.talesofarterra.model.Character;
+import byui.cit260.talesofarterra.control.CharacterControl;
+/**
+ *
+ * @author Lucas
+ */
+public class RestView extends View {
+             
+
+    public RestView() {
+       super("\n****************************************************************"
+           + "\n*                       REST MENU                              *"
+           + "\n   YOU ARE FULLY HEALED AND HEALTHY! HOPE TO SEE YOU AGAIN!    *"
+           + "\n****************************************************************");
+            }
+
+ @Override
+    public boolean doAction(String value)
+    {
+        Character fullHealCharacter = new Character();
+        CharacterControl setFullHeal = new CharacterControl();
+        setFullHeal.healFull(fullHealCharacter);
+         //maxHPChar = true;
+         
+         return true;
+    }
+    
+}
+
