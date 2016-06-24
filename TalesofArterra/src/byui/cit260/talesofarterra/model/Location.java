a9bae3132b7c449e7690465537d07561d02ea3bf
Dale <Dale@192.168.108.92>
2016-06-23 21:46:45 -0500

    Setup displayMap() function

--- TalesofArterra/src/byui/cit260/talesofarterra/model/Location.java
+++ TalesofArterra/src/byui/cit260/talesofarterra/model/Location.java
@@ -10,25 +10,27 @@ import java.util.Arrays;
 public enum Location implements Serializable {
     
     //list of Locations
-    OutsideStart(0,2,1,false,"Starting Location"),
-    OutsideNorthGate(0,2,4,false,"North Gate to Edinburg"),
-    CityCenterNorthGate(1,4,1,false,"Inside North Gate to Edinburg"),
-    CityCenterBedimes(1,5,2,false,"A noble couple and their servant"),
-    CityCenterEastGate(1,9,4,false,"Gate to Slums"), 
-    CityCenterSouthGate(1,7,9,false,"Gate to Merchant District"),
-    CityCenterWestGate(1,0,7,false,"Gate to Government District"),
-    CityCenterStore(1,2,3,false,"Samuel's Smith & Sundries"),
-    CityCenterPub(1,4,4,false,"Lazy Lion Pub");
+    OutsideStart(0,1,2,1,false,"Starting Location"),
+    OutsideNorthGate(0,2,2,4,false,"North Gate to Edinburg"),
+    CityCenterNorthGate(1,1,4,1,false,"Inside North Gate to Edinburg"),
+    CityCenterBedimes(1,2,5,2,false,"A noble couple and their servant"),
+    CityCenterEastGate(1,3,9,4,false,"Gate to Slums"), 
+    CityCenterSouthGate(1,4,7,9,false,"Gate to Merchant District"),
+    CityCenterWestGate(1,5,0,7,false,"Gate to Government District"),
+    CityCenterStore(1,6,2,3,false,"Samuel's Smith & Sundries"),
+    CityCenterPub(1,7,4,4,false,"Lazy Lion Pub");
 
     //private member variables
     private final int mapID;
+    private final int locationID;
     private final int[] coordinates;
     private final boolean blocked;
     private final String description;
 
     //public getter and setter functions
-    private Location(int mapID, int row, int column, boolean blocked, String description) {
+    private Location(int mapID, int locationID, int row, int column, boolean blocked, String description) {
         this.mapID = mapID;
+        this.locationID = locationID;
         int [] coord = new int[] {row,column};
         this.coordinates = coord;
         this.blocked = blocked;
@@ -37,6 +39,10 @@ public enum Location implements Serializable {
 
     public int getMapID() {
         return mapID;
+    }
+
+    public int getLocationID() {
+        return locationID;
     }
 
     public int[] getCoordinates() {
--- TalesofArterra/src/byui/cit260/talesofarterra/view/GameMenuView.java
+++ TalesofArterra/src/byui/cit260/talesofarterra/view/GameMenuView.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package byui.cit260.talesofarterra.view;
-import byui.cit260.talesofarterra.view.RestView;
+import byui.cit260.talesofarterra.model.Game;
+import byui.cit260.talesofarterra.model.Location;
+import talesofarterra.TalesofArterra;
 /**
  *
  * @author Portal
@@ -42,7 +44,7 @@ public boolean doAction(String choice) {
            {
             
             case "X": 
-                this.explore();
+                this.displayMap();
                 break;
            
             case "R": 
@@ -84,8 +86,62 @@ public boolean doAction(String choice) {
 
     }
 
-    private void explore() {
-        System.out.println("\n*** createNewGame stub function called ***");
+    private void displayMap() {
+        Game game = TalesofArterra.getGame();
+        
+        // get the map's dimensions
+        int rows = game.getCurrentMap().getSize()[0];
+        int cols = game.getCurrentMap().getSize()[1];
+        
+        //create a two-dimensional array of the Locations on the map
+        Location[][] locs = new Location[rows][cols];
+        for (Location loc : Location.values()) {
+            if (loc.getMapID() == game.getCurrentMap().getMapID()) {
+                int r = loc.getCoordinates()[1];
+                int c = loc.getCoordinates()[0];
+                locs[r][c] = loc;
+            }
+        }
+        
+        System.out.println(game.getCurrentMap().name() + "\n");
+        for (int i = 0; i < rows; i++) {
+            System.out.print("|");
+            for (int j = 0; j < cols; j++) {
+                // print location ID if a location is on the map
+                if (locs[i][j] == null) {
+                    System.out.print(" ");
+                }
+                else {
+                    // print X if current location
+                    if (locs[i][j] != game.getCurrentLocation()) {
+                        System.out.print(locs[i][j].getLocationID());
+                    }
+                    else {
+                        System.out.print("X");
+                    }
+                }
+                // print a column separator
+                System.out.print("|");
+                // print end line
+                if (j == cols - 1) {
+                    System.out.print("\n");
+                }
+            }
+            // print row separator
+            System.out.print("|");
+            for (int j = 0; j < cols; j++) {
+                System.out.print("--");
+            }
+            System.out.print("\n");
+        }
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (locs[i][j] != game.getCurrentLocation() && locs[i][j] != null) {
+                    System.out.println(locs[i][j].getLocationID() + " - " + locs[i][j].getDescription());
+                }
+            }
+        }
     }
 
     private void rest() {
@@ -94,7 +150,7 @@ public boolean doAction(String choice) {
         restView.doAction("playerChar.ser");
     }
     private void bagContent() {
-        System.out.println("\n*** createNewGame stub function called ***");
+        System.out.println("\n*** bagContent stub function called ***");
     }
 
     private void charSheet() {
