3c4dd20ca64c99d51303ba260a2da87fe032bfad
RodgersLucas <rod15031@byui.edu>
2016-06-29 00:57:26 -0600

    Added Store Inventory Class

--- TalesofArterra/src/byui/cit260/talesofarterra/control/SceneControl.java
+++ TalesofArterra/src/byui/cit260/talesofarterra/control/SceneControl.java
@@ -6,8 +6,10 @@
 package byui.cit260.talesofarterra.control;
  
 import byui.cit260.talesofarterra.model.Character;
+import java.text.DecimalFormat;
 //import byui.cit260.talesofarterra.model.Scene;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -40,7 +42,68 @@ public class SceneControl {
         {
             return false;
         }
-        
+        //possible monatery symbols
+        //₳₴₴₵₶₷₸₹€₡₣₢₤₥₦₧₨₩₪€₭₮₰₱₲ⅎ
  
+    }
+    public void storeInventory (){            
+        
+        int userPurchase; 
+
+        //This is the stock ammount of each item
+        int[] itemAmount = {8, 3, 2, 5, 9, 1}; 
+        //This is each item's name
+        String[] itemName = {"Regular Sword", "Strong Sword", "Dagger of the Night", 
+            "Potion of Healing", "Potion of Fortitude", "Shield of Olympus"};
+        //This is each item's price
+        float[] itemPrice = {10, 25, 78, 5, 9, 120};
+        //This it the total price of their order
+        float sum = 0;
+        //This is so the prices will have two zeros at the end of them 
+        DecimalFormat df = new DecimalFormat("#.00");
+        //This adds a number for each item for the user to order.
+        int itemNumber = 1;
+        System.out.print(
+                "\n****************************************************************"
+              + "\n*             WELCOME TO THE STORE IN EDINBURG                 *"
+              + "\n*                  *Business is Booming!*                      *");        
+        do{
+            
+            System.out.println(
+                    "\n*--------------------------------------------------------------*"                    
+                  + "\n*            The following is a list of our inventory          *" 
+                  + "\n****************************************************************");
+            for (int i = 0; i < itemAmount.length; i++)
+            {
+                System.out.println(itemNumber + " - " + itemName[i] + " - we have " + itemAmount[i] + " at ₢" + df.format(itemPrice[i]) + " each.");
+                itemNumber++;
+            }
+            System.out.println(
+                    "\n****************************************************************"
+                  + "\n*                What would you like to buy?                   *" 
+                  + "\n****************************************************************\n");
+            Scanner in = new Scanner(System.in);
+            System.out.println("Pick an item: (1 for the first, 2 for the second, so on and so forth");
+            System.out.print("Press 0 to leave\n>");
+            userPurchase = in.nextInt();
+            if (userPurchase != 0){  
+               
+                    //Trying to get it so it will not allow more than what is offered. 
+
+               // if (itemAmount.length < userPurchase){  
+               //     System.out.println("Test");
+               // } else {
+               //     System.out.println("Invalid Selection, Please select a number between 1 and " + itemAmount.length);}
+                if (itemAmount[userPurchase - 1] != 0){
+                 itemAmount[userPurchase - 1] -= 1;
+                 System.out.println("You just bought a " + itemName[userPurchase - 1] + "! Congrats!!\n");
+                 sum += itemPrice[userPurchase - 1];
+                    } else {
+                            System.out.println("We are Sold out! Sorry!!");}
+            }
+            itemNumber = 1;
+        }
+        while (userPurchase != 0);
+        System.out.println("You spent a total of ₢" + df.format(sum));
     }
 }
